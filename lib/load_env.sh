c376606f944c190cf63f394947ffb25324f6ca83
smahraz <smahraz@student.1337.ma>
2026-03-30 16:52:33 +0100

    chore: Change default installation path

--- lib/load_env.sh
+++ lib/load_env.sh
@@ -1,13 +1,13 @@
 if [ -z $TEMP_DIR ]; then
-  export TEMP_DIR="$HOME/goinfre/42_tmp"
+  export TEMP_DIR="$HOME/.42_tmp"
 fi
 
 if [ -z $INSTALL_PATH ]; then
-  export INSTALL_PATH="$HOME/goinfre/42_pkg"
+  export INSTALL_PATH="$HOME/.local"
 fi
 
 export BIN_PATH="$INSTALL_PATH/bin"
-export BIN_FILES_PATH="$INSTALL_PATH/share"
+export BIN_FILES_PATH="$INSTALL_PATH/lib"
 
 mkdir -p $TEMP_DIR $BIN_PATH $BIN_FILES_PATH
 
